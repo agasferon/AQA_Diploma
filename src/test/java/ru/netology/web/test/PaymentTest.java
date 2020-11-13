538895c27adc8639ab067bfe29c505520ba75f87
Anatoly Malyshev <anatoliym67@gmail.com>
2020-11-13 10:34:01 +0300

    edit CI configs

--- src/test/java/ru/netology/web/test/PaymentTest.java
+++ src/test/java/ru/netology/web/test/PaymentTest.java
@@ -45,94 +45,94 @@ class PaymentTest {
         assertEquals("APPROVED", DataBaseHelper.checkStatusInPayment());
     }
 
-//    @Test
-//    public void shouldNotPayByCard2() {
-//        val cardPaymentPage = enterToPaymentPage();
-//        cardPaymentPage.setAllValues(DataHelper.getDataWithCard2Number());
-//        cardPaymentPage.continueButton.click();
-//        cardPaymentPage.notificationError.waitUntil(Condition.visible,15000);
-//        assertEquals("DECLINED", DataBaseHelper.checkStatusInPayment());
-//    }
+    @Test
+    public void shouldNotPayByCard2() {
+        val cardPaymentPage = enterToPaymentPage();
+        cardPaymentPage.setAllValues(DataHelper.getDataWithCard2Number());
+        cardPaymentPage.continueButton.click();
+        cardPaymentPage.notificationError.waitUntil(Condition.visible,15000);
+        assertEquals("DECLINED", DataBaseHelper.checkStatusInPayment());
+    }
+
+    @Test
+    public void shouldNotPayByNotProvidedCard() {
+        val cardPaymentPage = enterToPaymentPage();
+        cardPaymentPage.setAllValues(DataHelper.getDataWithNotProvidedCardNumber());
+        cardPaymentPage.continueButton.click();
+        cardPaymentPage.notificationError.waitUntil(Condition.visible,15000);
+        assertEquals(null, DataBaseHelper.checkStatusInPayment());
+    }
 //
-//    @Test
-//    public void shouldNotPayByNotProvidedCard() {
-//        val cardPaymentPage = enterToPaymentPage();
-//        cardPaymentPage.setAllValues(DataHelper.getDataWithNotProvidedCardNumber());
-//        cardPaymentPage.continueButton.click();
-//        cardPaymentPage.notificationError.waitUntil(Condition.visible,15000);
-//        assertEquals(null, DataBaseHelper.checkStatusInPayment());
-//    }
-////
-//    @Test
-//    public void shouldNotPayByInvalidCard() {
-//        val cardPaymentPage = enterToPaymentPage();
-//        cardPaymentPage.setAllValues(DataHelper.getDataWithInvalidCardNumber());
-//        cardPaymentPage.continueButton.click();
-//        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
-//        assertEquals(null, DataBaseHelper.checkStatusInPayment());
-//    }
-//
-//    @Test
-//    public void shouldNotPayByExpiredCardThisYear() {
-//        val cardPaymentPage = enterToPaymentPage();
-//        cardPaymentPage.setAllValues(DataHelper.getDataWithExpiredDateCardThisYear());
-//        cardPaymentPage.continueButton.click();
-//        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
-//        assertEquals(null, DataBaseHelper.checkStatusInPayment());
-//    }
-//
-//    @Test
-//    public void shouldNotPayByExpiredCardLastYear() {
-//        val cardPaymentPage = enterToPaymentPage();
-//        cardPaymentPage.setAllValues(DataHelper.getDataWithExpiredDateCardLastYear());
-//        cardPaymentPage.continueButton.click();
-//        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
-//        assertEquals(null, DataBaseHelper.checkStatusInPayment());
-//    }
-//
-//    @Test
-//    public void shouldNotPayByInvalidMonthNumber() {
-//        val cardPaymentPage = enterToPaymentPage();
-//        cardPaymentPage.setAllValues(DataHelper.getDataWithInvalidMonthNumber());
-//        cardPaymentPage.continueButton.click();
-//        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
-//        assertEquals(null, DataBaseHelper.checkStatusInPayment());
-//    }
-//
-//    @Test
-//    public void shouldNotPayByZeroMonthNumber() {
-//        val cardPaymentPage = enterToPaymentPage();
-//        cardPaymentPage.setAllValues(DataHelper.getDataWithZeroMonthNumber());
-//        cardPaymentPage.continueButton.click();
-//        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
-//        assertEquals(null, DataBaseHelper.checkStatusInPayment());
-//    }
-//
-//    @Test
-//    public void shouldNotPayByInvalidYearNumber() {
-//        val cardPaymentPage = enterToPaymentPage();
-//        cardPaymentPage.setAllValues(DataHelper.getDataWithInvalidYearNumber());
-//        cardPaymentPage.continueButton.click();
-//        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
-//        assertEquals(null, DataBaseHelper.checkStatusInPayment());
-//    }
-//
-//    @Test
-//    public void shouldNotPayByCyrillicCardOwner() {
-//        val cardPaymentPage = enterToPaymentPage();
-//        cardPaymentPage.setAllValues(DataHelper.getDataWithCyrillicCardOwner());
-//        cardPaymentPage.continueButton.click();
-//        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
-//        assertEquals(null, DataBaseHelper.checkStatusInPayment());
-//    }
-//
-//    @Test
-//    public void shouldNotPayByInvalidCVC() {
-//        val cardPaymentPage = enterToPaymentPage();
-//        cardPaymentPage.setAllValues(DataHelper.getDataWithInvalidCVC());
-//        cardPaymentPage.continueButton.click();
-//        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
-//        assertEquals(null, DataBaseHelper.checkStatusInPayment());
-//    }
+    @Test
+    public void shouldNotPayByInvalidCard() {
+        val cardPaymentPage = enterToPaymentPage();
+        cardPaymentPage.setAllValues(DataHelper.getDataWithInvalidCardNumber());
+        cardPaymentPage.continueButton.click();
+        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
+        assertEquals(null, DataBaseHelper.checkStatusInPayment());
+    }
+
+    @Test
+    public void shouldNotPayByExpiredCardThisYear() {
+        val cardPaymentPage = enterToPaymentPage();
+        cardPaymentPage.setAllValues(DataHelper.getDataWithExpiredDateCardThisYear());
+        cardPaymentPage.continueButton.click();
+        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
+        assertEquals(null, DataBaseHelper.checkStatusInPayment());
+    }
+
+    @Test
+    public void shouldNotPayByExpiredCardLastYear() {
+        val cardPaymentPage = enterToPaymentPage();
+        cardPaymentPage.setAllValues(DataHelper.getDataWithExpiredDateCardLastYear());
+        cardPaymentPage.continueButton.click();
+        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
+        assertEquals(null, DataBaseHelper.checkStatusInPayment());
+    }
+
+    @Test
+    public void shouldNotPayByInvalidMonthNumber() {
+        val cardPaymentPage = enterToPaymentPage();
+        cardPaymentPage.setAllValues(DataHelper.getDataWithInvalidMonthNumber());
+        cardPaymentPage.continueButton.click();
+        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
+        assertEquals(null, DataBaseHelper.checkStatusInPayment());
+    }
+
+    @Test
+    public void shouldNotPayByZeroMonthNumber() {
+        val cardPaymentPage = enterToPaymentPage();
+        cardPaymentPage.setAllValues(DataHelper.getDataWithZeroMonthNumber());
+        cardPaymentPage.continueButton.click();
+        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
+        assertEquals(null, DataBaseHelper.checkStatusInPayment());
+    }
+
+    @Test
+    public void shouldNotPayByInvalidYearNumber() {
+        val cardPaymentPage = enterToPaymentPage();
+        cardPaymentPage.setAllValues(DataHelper.getDataWithInvalidYearNumber());
+        cardPaymentPage.continueButton.click();
+        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
+        assertEquals(null, DataBaseHelper.checkStatusInPayment());
+    }
+
+    @Test
+    public void shouldNotPayByCyrillicCardOwner() {
+        val cardPaymentPage = enterToPaymentPage();
+        cardPaymentPage.setAllValues(DataHelper.getDataWithCyrillicCardOwner());
+        cardPaymentPage.continueButton.click();
+        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
+        assertEquals(null, DataBaseHelper.checkStatusInPayment());
+    }
+
+    @Test
+    public void shouldNotPayByInvalidCVC() {
+        val cardPaymentPage = enterToPaymentPage();
+        cardPaymentPage.setAllValues(DataHelper.getDataWithInvalidCVC());
+        cardPaymentPage.continueButton.click();
+        cardPaymentPage.inputInvalid.waitUntil(Condition.visible,15000);
+        assertEquals(null, DataBaseHelper.checkStatusInPayment());
+    }
 
 }
